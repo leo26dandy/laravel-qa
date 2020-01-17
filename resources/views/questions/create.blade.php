b6347249233c67a0cc00bc3074584835fdfff630
dandy4satrio <dandy4satrio@gmail.com>
2020-01-17 13:37:19 +0700

    build question system based on user feddback with success message

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -19,19 +19,19 @@
                         @csrf
                         <div class="form-group">
                           <label for="question-title">Title</label>
-                          <input type="text" name="title" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}" placeholder="Typing down the title here.">
+                          <input type="text" name="title" id="question-title" value="{{ old('title')}}" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}" placeholder="Typing down the title here.">
                           @if ($errors->has('title'))
                               <div class="invalid-feedback">
-                                  <strong>{{ $errors->has('title') }}</strong>
+                                  <strong>{{ $errors->first('title') }}</strong>
                               </div>
                           @endif
                         </div>
                         <div class="form-group">
                           <label for="question-body">Question</label>
-                          <textarea name="body" id="question-body" cols="30" rows="10" placeholder="Typing down your question here." class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"></textarea>
+                          <textarea name="body" id="question-body" cols="30" rows="10" placeholder="Typing down your question here." class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}">{{ old('body')}}</textarea>
                             @if ($errors->has('body'))
                                 <div class="invalid-feedback">
-                                    <strong>{{ $errors->has('body') }}</strong>
+                                    <strong>{{ $errors->first('body') }}</strong>
                                 </div>
                             @endif
                         </div>
